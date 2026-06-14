9495e8eda1fea367f13bc0b4a830ce901fd21149
Alvin Labora <alvin@lapapa88.com>
2026-06-14 22:56:27 +0800

    Initial commit

---
+++ src/main/java/io/mentora/mentora_bot/MentoraBotApplication.java
@@ -0,0 +1,13 @@
+package io.mentora.mentora_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoraBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoraBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/mentora/mentora_bot/MentoraBotApplicationTests.java
@@ -0,0 +1,13 @@
+package io.mentora.mentora_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoraBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
